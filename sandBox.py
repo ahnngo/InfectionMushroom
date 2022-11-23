2cfff10d538c2e52c2761b9cbe3dfb1974727102
Anh Ngo <helloanhngo2002@gmail.com>
2022-11-22 19:35:55 -0500

    Adding healing probability

--- sandBox.py
+++ sandBox.py
@@ -1,6 +1,6 @@
 import copy
 import random
-INFECTED = 6
+INFECTED = 2
 
 
 class Cell:
@@ -44,6 +44,8 @@ class InfectedCell(Cell):
     def heal(self):
         self.infected -= 1
 
+    def get_infected_rate(self):
+        return self.infected
 
 class Skin:
 
@@ -70,6 +72,19 @@ class Skin:
             visual_skin.append(sub)
         return visual_skin
 
+    def get_infected_score(self):
+        visual_skin = []
+        for i in range(self.size):
+            sub = []
+            for j in range(self.size):
+                if self.skin[i][j].get_status() == '*':
+                    sub.append(self.skin[i][j].get_infected_rate())
+                else:
+                    sub.append(self.skin[i][j].get_status())
+            visual_skin.append(sub)
+        return visual_skin
+
+
     def get_infected(self, infected_rate=0.5):
         identical_skin = self.get_visual()
 
@@ -77,35 +92,44 @@ class Skin:
             print(i)
         print()
 
+        identical_skin_score = self.get_infected_score()
+
+        for i in identical_skin_score:
+            print(i)
+        print()
+
         nbrs = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         for row in range(self.size):
             for col in range(self.size):
                 infected_nbrs = 0
+                if identical_skin[row][col] == '#':
+                    break
                 for nbr in nbrs:
                     r = row + nbr[0]
                     c = col + nbr[1]
                     if (0 <= r < self.size) and (0 <= c < self.size) and identical_skin[r][c] == "*":
                         infected_nbrs += 1
 
-                if infected_nbrs == 0 and identical_skin[row][col] != "*":
-                    self.skin[row][col] = HealthyCell(row, col)
+                if identical_skin[row][col] == "*":
+                    healing_chance = random.uniform(0, 1)
+                    if healing_chance > (infected_nbrs / 8):
+                        self.skin[row][col].heal()
+                    if self.skin[row][col].get_infected_rate() == 0:
+                        self.skin[row][col] = ImmuneCell(row, col)
                 else:
-                    infected_chance = random.uniform(0, 0.4) + float(infected_nbrs) * 20/100
-                    if infected_chance > 0.5:
-                        self.skin[row][col] = InfectedCell(row, col)
+                    if infected_nbrs == 0:
+                        self.skin[row][col] = HealthyCell(row, col)
+                    else:
+                        infected_chance = random.uniform(0, 1)
+                        if 1 - infected_chance < (infected_nbrs / 8):
+                            self.skin[row][col] = InfectedCell(row, col)
+
 
 
 myskin = Skin(5)
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
-myskin.get_infected()
+for i in range(20):
+    myskin.get_infected()
+    myskin.get_infected_score()
 
 
 
