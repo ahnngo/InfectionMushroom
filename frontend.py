1ea58574df17dd1c629da206e9813556d31a7e91
kharela <Kharela@berea.edu>
2022-12-16 00:30:30 -0500

    removed duplicate file

--- frontend.py
+++ frontend.py
@@ -4,7 +4,7 @@
 ###################################################################
 from time import sleep
 from tkinter import *
-import backend as logic
+import sandBox as logic
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
@@ -12,9 +12,9 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 
 class Gui:
     def __init__(self):
-        self.root = Tk()
-        self.root.state('zoomed')
-        self.drawWindow(self.root)
+        self.root = Tk()  # establish a base root window to put everything on
+        self.root.state('zoomed') # full screen
+        self.drawWindow(self.root) # initial call to draw function
         self.graph = None
 
 
@@ -25,12 +25,12 @@ class Gui:
         title = Label(root, text='Infection Simulator',font=('sans',50)).grid(row=0,column=0,columnspan=3)
 
         paramframe = LabelFrame(root,width=450,height=400,highlightcolor='Red',relief='raised',pady=15,padx=10)
-        paramframe.grid(row=1,column=0,pady=75,padx=120,sticky='n')
+        paramframe.grid(row=1,column=0,pady=100,padx=120,sticky='n')
 
         canvas = Canvas(root).grid(row=1,column=1)
 
 
-        param_head = Label(paramframe,text='Parameters',font=('times new roman',30)).grid(row=0,column=0,columnspan=3)
+        param_head = Label(paramframe,text='Parameters',font=('times new roman',30)).grid(row=0,column=0,columnspan=3)    #Establishes the initial state of the screen with sliders and entry objects
         ir_label = Label(paramframe,text= 'Infection Rate:',font=('times new roman',20)).grid(row=1,column=0,pady=10)
         ir_slider = Scale(paramframe, from_=0, to=100, orient=HORIZONTAL,length=150,tickinterval=25)
         ir_slider.grid(row=1,column=1,padx=15,columnspan=2)
@@ -44,15 +44,20 @@ class Gui:
         days_input = Entry(paramframe, font=('times new roman', 12), justify='center')
         days_input.grid(row=4, column=1, padx=15, columnspan=2)
 
-        start = Button(root,text='Simulate',font=('times new roman',30),bg='green',command=lambda: startsimulation(int(size_input.get()), hr_slider.get(), ir_slider.get(),self,int(days_input.get()))).grid(row=3,column=0,stick='n')
-
+        start = Button(root,text='Simulate',font=('times new roman',30),bg='green',command=lambda: startsimulation(int(size_input.get()), hr_slider.get(), ir_slider.get(),self,int(days_input.get()))).grid(row=2,column=0,stick='n')
+        # button to start the simulation which calls the atartsimulation function on click.
 
         self.graph = canvas
 
+    def update_animate(self,visual_skin):
+        '''
+        Updates the matplotb by re-processing the skins output and converting into a matplotlib
+        which is converted into a tkinter canvas object then put on the screen
+        :param visual_skin: The skin matrix
+        :return:
+        '''
 
-    def update_animate(self,visual_skin, day):
-
-        for i in range(len(visual_skin)):
+        for i in range(len(visual_skin)):              #Converts the character matrix into a numbers one
             for j in range(len(visual_skin)):
                 if visual_skin[i][j] == '.':
                     visual_skin[i][j] = int(0)
@@ -62,28 +67,38 @@ class Gui:
                     visual_skin[i][j] = int(2)
 
         visual_skin = np.array(visual_skin)
-        colors = np.array([[0, 255, 0],  # green
+        colors = np.array([[0, 255, 0],  # green        #Establishes the color scheme by index number
                            [255, 0, 0],  # red
                            [220, 220, 220]])  # gray
 
-        plotfin = colors[visual_skin]
+        plotfin = colors[visual_skin]       #Creates a matlib plot with the visual skin using the color established above
         fig, ax = plt.subplots()
+        # ax.plot(plotfin)
 
         ax.imshow(plotfin)
-        plt.title('Day: ' + str(day))
         canvas = plt.Figure()
-        canvas = FigureCanvasTkAgg(fig,
+        canvas = FigureCanvasTkAgg(fig,                             #Converts the matplotlib plot object into a canvas
                                   master=self.root)
         canvas.get_tk_widget().grid(row=1,column=1,padx=15,pady=20,sticky='w')
-        self.root.update()
+        # self.graph.draw()
+        self.root.update()      #Refreshes the page so the new matrix can be displayed
+        # plt.show()
 def startsimulation(size,healingrate,infectionrate,gui,days):
+    '''
+    Button click function which takes all the input data and starts the simulation process
+    :param size: Size of skin matrix
+    :param healingrate: The rate of cells that get healed
+    :param infectionrate: the rate of infection on non-immune cells
+    :param gui: The GUI objects which we will call the functions on
+    :param days: THe length of simulation in days
+    :return:
+    '''
 
-    simulation = logic.Skin(size=size,infected_rate=infectionrate/100,healing_rate=healingrate/100)
-    print(infectionrate,healingrate)
+    simulation = logic.Skin(size=size,infected_rate=infectionrate//100,healing_rate=healingrate) # create a skin object based on parameters
     for i in range(days+1):
-        simulation.get_infected()
-        gui.update_animate(simulation.get_visual().tolist(), i)
-
+        simulation.get_infected()       #Steps to the next day in the simulations and get the new skin matrix
+        gui.update_animate(simulation.get_visual().tolist())  #Gives the skin matrix we got above and updates the GUI accordingly
+        # sleep(.2)
 
 
 
